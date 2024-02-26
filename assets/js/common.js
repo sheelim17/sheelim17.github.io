811f16b9b6ad592807c97bb20ddc27f17a48782e
sheelim17 <103024794+sheelim17@users.noreply.github.com>
2024-02-26 21:44:10 +0900

    test

---
+++ assets/js/common.js
@@ -0,0 +1,73 @@
+$(function() {
+    const wrapper = $('.wrap')
+    let scrollTop = 0;
+    
+    // 약관 팝업 열기 
+    $('.open-layer').each(function() {
+        let target = $(this).attr('data-target')
+        $(this).on('click', function() {
+            $(target).css('display', 'block')
+        }) 
+    })
+    // 약관 팝업 닫기
+    $('.layer-full').each(function() {
+        let cls = $(this).find('.layer-cls')
+        cls.each(function() {
+            let target = $(this).attr('data-target')
+            $(this).on('click', function() {
+                $(target).css('display', 'none')
+            }) 
+        })
+    })
+     // bottom layer 열기
+    $('.open-btm-layer').each(function() {
+        let target = $(this).attr('data-target');
+        $(this).on('click', function() {
+            scrollTop = window.scrollY;
+            $(target).addClass('show')
+            $('body').addClass('scroll-block');
+            wrapper.scrollTop(scrollTop)
+        }) 
+    })
+     // bottom layer 닫기
+     $('.btm-layer-wrap').each(function() {
+        let cls = $(this).find('.layer-cls')
+        cls.on('click', function() {
+            $(this).parents('.btm-layer-wrap').removeClass('show');
+            $('body').removeClass('scroll-block');
+            wrapper.scrollTop(scrollTop)
+        }) 
+    })
+
+    $('.pet-item').each(function() {
+        let item = $(this)
+        let selBtn = item.find('.btn-sel');
+        selBtn.on('click', function() {
+            item.toggleClass('on')
+        }) 
+    })
+
+    $('.tooltip').each(function(){
+        let self = $(this)
+        let btn = $(this).find('.btn-tooltip');
+        let clsBtn = $(this).find('.btn-cls')
+        btn.on('click', function() {
+            self.addClass('show')
+        })
+        clsBtn.on('click', function() {
+            self.removeClass('show')
+        })
+    })
+    
+    
+
+})
+ // message box 보여주기 위해 임시로 넣어둔 이벤트 입니다. 
+function showMessageBox() {
+    $('.message-bar').animate({ bottom : '20px'}, 800, function() {
+        $( this ).delay(1000).animate( {
+          bottom: '-100%'
+        }, 1000 );
+      } 
+    )
+}
