033de39f2d0a176427d78bf1177f4b89ced484bf
sheelim17 <103024794+sheelim17@users.noreply.github.com>
2024-04-02 19:51:49 +0900

    petpass new

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,73 +1,26 @@
+const wrapper = $('.wrap')
+let scrollPosition = 0;
+
 $(function() {
-    const wrapper = $('.wrap')
-    let scrollPosition = 0;
-    
-    // 약관 팝업 열기 
-    $('.open-layer').each(function() {
-        let target = $(this).attr('data-target')
-        $(this).on('click', function() {
-            $(target).css('display', 'block')
+    $('.custom-select').each(function() {
+        let $select = $(this)
+        $select.on('click', function() {
+            $select.toggleClass('active')
         }) 
     })
-    // 약관 팝업 닫기
-    $('.layer-full').each(function() {
-        let cls = $(this).find('.layer-cls')
-        cls.each(function() {
-            let target = $(this).attr('data-target')
-            $(this).on('click', function() {
-                $(target).css('display', 'none')
-            }) 
-        })
-    })
-     // bottom layer 열기
-    $('.open-btm-layer').each(function() {
-        let target = $(this).attr('data-target');
-        $(this).on('click', function() {
-            scrollPosition = window.scrollY;
-            $(target).addClass('show')
-            $('body').addClass('scroll-block');
-            wrapper.scrollTop(scrollPosition);
-        }) 
-    })
-     // bottom layer 닫기
-     $('.btm-layer-wrap').each(function() {
-        let cls = $(this).find('.layer-cls')
-        cls.on('click', function() {
-            $(this).parents('.btm-layer-wrap').removeClass('show');
-            $('body').removeClass('scroll-block');
-            $(window).scrollTop(scrollPosition);
-        });
-    })
-
-    $('.pet-item').each(function() {
-        let item = $(this)
-        let selBtn = item.find('.btn-sel');
-        selBtn.on('click', function() {
-            item.toggleClass('on')
-        }) 
-    })
-
-    $('.tooltip').each(function(){
-        let self = $(this)
-        let btn = $(this).find('.btn-tooltip');
-        let clsBtn = $(this).find('.btn-cls')
-        btn.on('click', function() {
-            self.addClass('show')
-        })
-        clsBtn.on('click', function() {
-            self.removeClass('show')
-        })
-    })
-    
-    
-
 })
- // message box 보여주기 위해 임시로 넣어둔 이벤트 입니다. 
-function showMessageBox() {
-    $('.message-bar').animate({ bottom : '20px'}, 800, function() {
-        $( this ).delay(1000).animate( {
-          bottom: '-100%'
-        }, 1000 );
-      } 
-    )
+
+function openLayer(layerId) {
+    let $layerId = $('#'+ layerId)
+    scrollPosition = window.scrollY;
+    $layerId.css('display', 'flex');
+    $('body').addClass('scroll-block');
+    wrapper.scrollTop(scrollPosition);
+}
+
+function closeLayer(layerId) {
+    let $layerId = $('#'+ layerId)
+    $layerId.css('display', 'none');
+    $('body').removeClass('scroll-block');
+    $(window).scrollTop(scrollPosition);
 }
